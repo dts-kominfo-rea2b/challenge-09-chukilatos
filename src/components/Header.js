4930fa4ffa4aeba07582e096cc6d18144f788c60
chukilatos <chukilatos@gmail.com>
2022-06-19 15:36:11 +0700

    add contact.js, add header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import "./Header.css";
+import React from "react";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <div className="Header">
+        <h2>Call a Friend</h2>
+        <p>your friendly contact app</p>
+
+        <div style={{ textAlign: "center" }}>
+          <hr />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Header;
